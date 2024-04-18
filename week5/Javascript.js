4041f2ae48122b568ee5ed13f2ba2fa6ba981669
NoaLwx <130018250+NoaLwx@users.noreply.github.com>
2024-04-18 12:20:48 +1000

    aaaaa

--- week5/Javascript.js
+++ week5/Javascript.js
@@ -30,3 +30,26 @@ else if (temperature>30){
     console.log("it feels hot")
 }
 }
+
+let shoppingCart = [
+    {name: "T-shirt", price:20},
+    {name: "Jeans", price:50},
+    {name:"Sneakers",price:80},
+    {name: "Backpack",price: 30}
+];
+
+let total = 0;
+for (let i = 0; i<4; i++) {
+    total = total + shoppingCart [i].price;
+    console.log ("the sum so far", total);
+}
+
+console.log("total rpice", total);
+
+let discount = 0.1;
+let discountPrice = 0;
+if (total > 100){
+    discountPrice = total - total*discount;
+}
+
+console.log("the discount price", discountPrice)
